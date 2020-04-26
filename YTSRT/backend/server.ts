db6d9e72fdd69299d8300e302cfc77d3368587e2
Felipe Garcia Diaz <41710468+felipeGarciaDiaz@users.noreply.github.com>
2020-04-26 02:56:50 -0400

    Update server.ts

--- YTSRT/backend/server.ts
+++ YTSRT/backend/server.ts
@@ -4,8 +4,8 @@ const express: any = require("express");
 const app: any = express();
 const http: any = require("http").createServer(app);
 const ioS: any = require("socket.io")(http);
-
 const readline: any = require("readline");
+
 const PORT: number = 42553;
 const ytVideo: any = readline.createInterface({
     input: process.stdin,
@@ -16,22 +16,27 @@ ytVideo.question("Please put in the video link here: ", (link: string): void => 
     video_link = link;
     ytVideo.close();
 });
-ytVideo.on("close", (): void => {
-    console.log("please go to to youre server to view the video");
 
+ytVideo.on("close", (): void => {
+    
+    console.log("please go to to your server to view the video");
     app.use("/", express.static(__dirname + "../client/"));
 
     ioS.on("connection", (socket: any): void => {
+        
         socket.emit("video", video_link);
-
         let updateStateHandler = (request: string, approval: string): void => {
+            
             socket.on(request, (data: string): void => {
                 ioS.emit(approval, data);
             });
+            
         };
+        
         updateStateHandler("playRequest", "playApprove");
         updateStateHandler("pauseRequest", "pauseApprove");
         updateStateHandler("seekRequest", "seekApprove");
+        
     });
 
     http.listen(PORT, () => {
